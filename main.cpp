84c2ea67b92d5685b1f357bc959478c03b2c1723
tihdarya <tihdasha06@gmail.com>
2020-11-20 23:40:35 +0300

    final version

--- main.cpp
+++ main.cpp
@@ -18,8 +18,7 @@ void encryption(std::vector<char> bytes, int shift, int key) {
         char b4 = i + 3 < bytes.size() ? bytes[i + 3] : 0u;
         unsigned int
                 xored_message = (static_cast<unsigned int>(b1) << 24u | static_cast<unsigned int>(b2) << 16u
-                                 | static_cast<unsigned int>(b3) << 8u | static_cast<unsigned int>(b4))
-        xorgamma;
+                                 | static_cast<unsigned int>(b3) << 8u | static_cast<unsigned int>(b4)) xor gamma;
         unsigned int shifted_message = (xored_message >> shift) | ((xored_message) << (32 - shift));
         b1 = shifted_message >> 24;
         ofs.write(&b1, sizeof(b1));
@@ -54,8 +53,7 @@ void decryption(int shift, int key) {
         unsigned int w4 = static_cast<unsigned int>(b4) & 0x000000ff;
         unsigned int decrypted_message = (w1 | w2 | w3 | w4);
         unsigned int shifted_message = (decrypted_message << shift) | ((decrypted_message) >> (32 - shift));
-        unsigned int result_message = shifted_message
-        xorgamma;
+        unsigned int result_message = shifted_message xor gamma;
         result[i] = result_message >> 24;
         result[i + 1] = result_message >> 16;
         result[i + 2] = result_message >> 8;
@@ -77,7 +75,7 @@ int main(int argc, const char *argv[]) {
         }
     }
     catch (const char *exception) {
-        std::cerr << "Error: " << exception << std::endl;
+        std::cout << "Error: " << exception << std::endl;
     }
 
     unsigned int key, shift = 3;
